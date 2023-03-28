8780e3dcc4e98050b31b2a3c10eabe11dce4d7dd
Achmed <ava@cleverreach.com>
2023-03-28 17:57:42 +0200

    extract dbus funcs

--- main.go
+++ main.go
@@ -4,17 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math/rand"
-	"os"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
+	"victron_energymeter_mqtt/dbustools"
 	"victron_energymeter_mqtt/phase"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"github.com/godbus/dbus/introspect"
-	"github.com/godbus/dbus/v5"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -36,50 +33,9 @@ var P3 float64 = 0.00
 var psum float64 = 0.00
 var psum_update bool = true
 var value_correction bool = false
-var conn, err = dbus.SystemBus()
 var dryrun bool
 var totalMessages uint32
 var logInterval int32
-
-const intro = `
-<node>
-   <interface name="com.victronenergy.BusItem">
-    <signal name="PropertiesChanged">
-      <arg type="a{sv}" name="properties" />
-    </signal>
-    <method name="SetValue">
-      <arg direction="in"  type="v" name="value" />
-      <arg direction="out" type="i" />
-    </method>
-    <method name="GetText">
-      <arg direction="out" type="s" />
-    </method>
-    <method name="GetValue">
-      <arg direction="out" type="v" />
-    </method>
-    </interface>` + introspect.IntrospectDataString + `</node> `
-
-type objectpath string
-
-var victronValues = map[int]map[objectpath]dbus.Variant{
-	// 0: This will be used to store the VALUE variant
-	0: map[objectpath]dbus.Variant{},
-	// 1: This will be used to store the STRING variant
-	1: map[objectpath]dbus.Variant{},
-}
-var victronValuesMutex = &sync.RWMutex{}
-
-func (f objectpath) GetValue() (dbus.Variant, *dbus.Error) {
-	log.Debug("GetValue() called for ", f)
-	log.Debug("...returning ", victronValues[0][f])
-	return victronValues[0][f], nil
-}
-func (f objectpath) GetText() (string, *dbus.Error) {
-	log.Debug("GetText() called for ", f)
-	log.Debug("...returning ", victronValues[1][f])
-	// Why does this end up ""SOMEVAL"" ... trim it I guess
-	return strings.Trim(victronValues[1][f].String(), "\""), nil
-}
 
 func init() {
 	log.SetFormatter(&log.TextFormatter{
@@ -118,9 +74,9 @@ func init() {
 	// spew.Dump()
 	//-----------------------------
 
-	dryrun = viper.GetBool("dryrun")
-	if dryrun {
+	if viper.GetBool("dryrun") {
 		log.Warn("dry run / dbus disabled")
+		dbustools.DryRun = true
 	}
 
 	switch viper.GetString("loglevel") {
@@ -186,8 +142,8 @@ func init() {
 }
 
 func main() {
-	connectDbus()
-	defer conn.Close()
+	dbustools.Connect()
+	defer dbustools.Close()
 
 	// mqtt.ERROR = llog.New(os.Stdout, "[ERROR] ", 0)
 	// mqtt.CRITICAL = llog.New(os.Stdout, "[CRIT] ", 0)
@@ -235,187 +191,6 @@ func sub(client mqtt.Client) {
 	token := client.Subscribe(topic, 1, nil)
 	token.Wait()
 	log.Info("Subscribed to topic: " + topic)
-}
-
-/* connect to DBUS */
-func connectDbus() {
-	// Need to implement following paths:
-	// https://github.com/victronenergy/venus/wiki/dbus#grid-meter
-	// also in system.py
-	victronValues[0]["/Connected"] = dbus.MakeVariant(1)
-	victronValues[1]["/Connected"] = dbus.MakeVariant("1")
-
-	victronValues[0]["/CustomName"] = dbus.MakeVariant("Grid meter")
-	victronValues[1]["/CustomName"] = dbus.MakeVariant("Grid meter")
-
-	victronValues[0]["/DeviceInstance"] = dbus.MakeVariant(30)
-	victronValues[1]["/DeviceInstance"] = dbus.MakeVariant("30")
-
-	// also in system.py
-	victronValues[0]["/DeviceType"] = dbus.MakeVariant(71)
-	victronValues[1]["/DeviceType"] = dbus.MakeVariant("71")
-
-	victronValues[0]["/ErrorCode"] = dbus.MakeVariantWithSignature(0, dbus.SignatureOf(123))
-	victronValues[1]["/ErrorCode"] = dbus.MakeVariant("0")
-
-	victronValues[0]["/FirmwareVersion"] = dbus.MakeVariant(2)
-	victronValues[1]["/FirmwareVersion"] = dbus.MakeVariant("2")
-
-	// also in system.py
-	victronValues[0]["/Mgmt/Connection"] = dbus.MakeVariant("/dev/ttyUSB0")
-	victronValues[1]["/Mgmt/Connection"] = dbus.MakeVariant("/dev/ttyUSB0")
-
-	victronValues[0]["/Mgmt/ProcessName"] = dbus.MakeVariant("/opt/color-control/dbus-cgwacs/dbus-cgwacs")
-	victronValues[1]["/Mgmt/ProcessName"] = dbus.MakeVariant("/opt/color-control/dbus-cgwacs/dbus-cgwacs")
-
-	victronValues[0]["/Mgmt/ProcessVersion"] = dbus.MakeVariant("1.8.0")
-	victronValues[1]["/Mgmt/ProcessVersion"] = dbus.MakeVariant("1.8.0")
-
-	victronValues[0]["/Position"] = dbus.MakeVariantWithSignature(0, dbus.SignatureOf(123))
-	victronValues[1]["/Position"] = dbus.MakeVariant("0")
-
-	// also in system.py
-	victronValues[0]["/ProductId"] = dbus.MakeVariant(45058)
-	victronValues[1]["/ProductId"] = dbus.MakeVariant("45058")
-
-	// also in system.py
-	victronValues[0]["/ProductName"] = dbus.MakeVariant("Grid meter")
-	victronValues[1]["/ProductName"] = dbus.MakeVariant("Grid meter")
-
-	victronValues[0]["/Serial"] = dbus.MakeVariant("BP98305081235")
-	victronValues[1]["/Serial"] = dbus.MakeVariant("BP98305081235")
-
-	// Provide some initial values... note that the values must be a valid formt otherwise dbus_systemcalc.py exits like this:
-	//@400000005ecc11bf3782b374   File "/opt/victronenergy/dbus-systemcalc-py/dbus_systemcalc.py", line 386, in _handletimertick
-	//@400000005ecc11bf37aa251c     self._updatevalues()
-	//@400000005ecc11bf380e74cc   File "/opt/victronenergy/dbus-systemcalc-py/dbus_systemcalc.py", line 678, in _updatevalues
-	//@400000005ecc11bf383ab4ec     c = _safeadd(c, p, pvpower)
-	//@400000005ecc11bf386c9674   File "/opt/victronenergy/dbus-systemcalc-py/sc_utils.py", line 13, in safeadd
-	//@400000005ecc11bf387b28ec     return sum(values) if values else None
-	//@400000005ecc11bf38b2bb7c TypeError: unsupported operand type(s) for +: 'int' and 'unicode'
-	//
-	victronValues[0]["/Ac/L1/Power"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L1/Power"] = dbus.MakeVariant("0 W")
-	victronValues[0]["/Ac/L2/Power"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L2/Power"] = dbus.MakeVariant("0 W")
-	victronValues[0]["/Ac/L3/Power"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L3/Power"] = dbus.MakeVariant("0 W")
-
-	victronValues[0]["/Ac/L1/Voltage"] = dbus.MakeVariant(230)
-	victronValues[1]["/Ac/L1/Voltage"] = dbus.MakeVariant("230 V")
-	victronValues[0]["/Ac/L2/Voltage"] = dbus.MakeVariant(230)
-	victronValues[1]["/Ac/L2/Voltage"] = dbus.MakeVariant("230 V")
-	victronValues[0]["/Ac/L3/Voltage"] = dbus.MakeVariant(230)
-	victronValues[1]["/Ac/L3/Voltage"] = dbus.MakeVariant("230 V")
-
-	victronValues[0]["/Ac/L1/Current"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L1/Current"] = dbus.MakeVariant("0 A")
-	victronValues[0]["/Ac/L2/Current"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L2/Current"] = dbus.MakeVariant("0 A")
-	victronValues[0]["/Ac/L3/Current"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L3/Current"] = dbus.MakeVariant("0 A")
-
-	victronValues[0]["/Ac/L1/Energy/Forward"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L1/Energy/Forward"] = dbus.MakeVariant("0 kWh")
-	victronValues[0]["/Ac/L2/Energy/Forward"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L2/Energy/Forward"] = dbus.MakeVariant("0 kWh")
-	victronValues[0]["/Ac/L3/Energy/Forward"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L3/Energy/Forward"] = dbus.MakeVariant("0 kWh")
-
-	victronValues[0]["/Ac/L1/Energy/Reverse"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L1/Energy/Reverse"] = dbus.MakeVariant("0 kWh")
-	victronValues[0]["/Ac/L2/Energy/Reverse"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L2/Energy/Reverse"] = dbus.MakeVariant("0 kWh")
-	victronValues[0]["/Ac/L3/Energy/Reverse"] = dbus.MakeVariant(0.0)
-	victronValues[1]["/Ac/L3/Energy/Reverse"] = dbus.MakeVariant("0 kWh")
-
-	basicPaths := []dbus.ObjectPath{
-		"/Connected",
-		"/CustomName",
-		"/DeviceInstance",
-		"/DeviceType",
-		"/ErrorCode",
-		"/FirmwareVersion",
-		"/Mgmt/Connection",
-		"/Mgmt/ProcessName",
-		"/Mgmt/ProcessVersion",
-		"/Position",
-		"/ProductId",
-		"/ProductName",
-		"/Serial",
-	}
-
-	updatingPaths := []dbus.ObjectPath{
-		"/Ac/L1/Power",
-		"/Ac/L2/Power",
-		"/Ac/L3/Power",
-		"/Ac/L1/Voltage",
-		"/Ac/L2/Voltage",
-		"/Ac/L3/Voltage",
-		"/Ac/L1/Current",
-		"/Ac/L2/Current",
-		"/Ac/L3/Current",
-		"/Ac/L1/Energy/Forward",
-		"/Ac/L2/Energy/Forward",
-		"/Ac/L3/Energy/Forward",
-		"/Ac/L1/Energy/Reverse",
-		"/Ac/L2/Energy/Reverse",
-		"/Ac/L3/Energy/Reverse",
-	}
-
-	// Some of the victron stuff requires it be called grid.cgwacs... using the only known valid value (from the simulator)
-	// This can _probably_ be changed as long as it matches com.victronenergy.grid.cgwacs_*
-	if !dryrun {
-		reply, err := conn.RequestName("com.victronenergy.grid.cgwacs_ttyUSB0_di30_mb1",
-			dbus.NameFlagDoNotQueue)
-		if err != nil {
-			log.Panic("Something went horribly wrong in the dbus connection")
-			panic(err)
-		}
-
-		if reply != dbus.RequestNameReplyPrimaryOwner {
-			log.Panic("name cgwacs_ttyUSB0_di30_mb1 already taken on dbus.")
-			os.Exit(1)
-		}
-	}
-
-	for i, s := range basicPaths {
-		log.Debug("Registering dbus basic path #", i, ": ", s)
-		if !dryrun {
-			conn.Export(objectpath(s), s, "com.victronenergy.BusItem")
-			conn.Export(introspect.Introspectable(intro), s, "org.freedesktop.DBus.Introspectable")
-		}
-	}
-
-	for i, s := range updatingPaths {
-		log.Debug("Registering dbus update path #", i, ": ", s)
-		if !dryrun {
-			conn.Export(objectpath(s), s, "com.victronenergy.BusItem")
-			conn.Export(introspect.Introspectable(intro), s, "org.freedesktop.DBus.Introspectable")
-		}
-	}
-
-}
-
-/* Write dbus Values to Victron handler */
-func updateVariant(value float64, unit string, path string) (err error) {
-	emit := make(map[string]dbus.Variant)
-	emit["Text"] = dbus.MakeVariant(fmt.Sprintf("%.2f", value) + unit)
-	emit["Value"] = dbus.MakeVariant(float64(value))
-	victronValuesMutex.Lock()
-	victronValues[0][objectpath(path)] = emit["Value"]
-	victronValues[1][objectpath(path)] = emit["Text"]
-	victronValuesMutex.Unlock()
-	if !dryrun {
-		err = conn.Emit(dbus.ObjectPath(path), "com.victronenergy.BusItem.PropertiesChanged", emit)
-	}
-	if err != nil {
-		log.WithFields(log.Fields{"path": path, "unit": unit, "value": value}).Warn("could not update dbus value")
-	} else {
-		log.WithFields(log.Fields{"path": path, "unit": unit, "value": value}).Trace("new dbus value")
-
-	}
-	return
 }
 
 /* Convert binary to float64 */
@@ -549,16 +324,16 @@ func UpdateDbus() {
 			"Imported": ph.Imported,
 		}).Debug("New MQTT values for " + ph.Name)
 
-		updateVariant(ph.Power, "W", "/Ac/"+ph.Name+"/Power")
-		updateVariant(ph.Current, "A", "/Ac/"+ph.Name+"/Current")
-		updateVariant(ph.Voltage, "V", "/Ac/"+ph.Name+"/Voltage")
-		updateVariant(ph.Exported, "kWh", "/Ac/"+ph.Name+"/Energy/Forward")
-		updateVariant(ph.Imported, "kWh", "/Ac/"+ph.Name+"/Energy/Reverse")
+		dbustools.Update(ph.Power, "W", "/Ac/"+ph.Name+"/Power")
+		dbustools.Update(ph.Current, "A", "/Ac/"+ph.Name+"/Current")
+		dbustools.Update(ph.Voltage, "V", "/Ac/"+ph.Name+"/Voltage")
+		dbustools.Update(ph.Exported, "kWh", "/Ac/"+ph.Name+"/Energy/Forward")
+		dbustools.Update(ph.Imported, "kWh", "/Ac/"+ph.Name+"/Energy/Reverse")
 
 	}
-	updateVariant(tKw, "W", "/Ac/Power")
-	updateVariant(tExported, "kWh", "/Ac/Energy/Forward")
-	updateVariant(tImported, "kWh", "/Ac/Energy/Reverse")
+	dbustools.Update(tKw, "W", "/Ac/Power")
+	dbustools.Update(tExported, "kWh", "/Ac/Energy/Forward")
+	dbustools.Update(tImported, "kWh", "/Ac/Energy/Reverse")
 
 }
 
